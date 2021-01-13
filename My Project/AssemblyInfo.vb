b043ca1d026e65b01054f6aa133c659d9f335039
Laura <laurakriley01@gmail.com>
2021-01-13 08:34:50 -0700

    Add project files.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Operators")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Operators")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("cbf1f2a1-b0fe-44f4-850f-f0c38af8e824")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ OperatorsExample.vb
@@ -0,0 +1,25 @@
+﻿Module OperatorsExample
+
+    Sub Main()
+
+        'Addition
+        Console.WriteLine(2 + 2)
+
+        'Subtraction
+        Console.WriteLine(8 - 2)
+
+        'Multiplication
+        Console.WriteLine(5 * 5)
+
+        'Division as Double (decimal number)
+        Console.WriteLine(1 / 2)
+        'Division as Integer (whole number)
+        Console.WriteLine(1 \ 2)
+        'Division gives only the remainder
+        Console.WriteLine(1 Mod 2)
+
+        Console.Read()
+
+    End Sub
+
+End Module
