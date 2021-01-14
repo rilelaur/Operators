b10732206785a6d5f0180c9ac7f559a61969a475
Laura <laurakriley01@gmail.com>
2021-01-14 08:41:36 -0700

    Added Comparisons

--- OperatorsExample.vb
+++ OperatorsExample.vb
@@ -1,4 +1,5 @@
-﻿Module OperatorsExample
+﻿Option Compare Binary
+Module OperatorsExample
 
     Sub Main()
 
@@ -17,6 +18,26 @@
         Console.WriteLine(1 \ 2)
         'Division gives only the remainder
         Console.WriteLine(1 Mod 2)
+
+        'Concatination
+        Console.WriteLine("Hello" & " World")
+
+
+        'Comparisons
+
+        'Boolean throws a true or false (here it is false)
+        Console.WriteLine(3 > 4)
+        'Boolean throws a true
+        Console.WriteLine(3 < 4)
+        'Boolean throws a false (3 is not equal to 4)
+        Console.WriteLine(3 = 4)
+        'Boolean throws a true (3 is not equal to 4)
+        Console.WriteLine(3 <> 4)
+        'Here it is comparing the ascii value of the strings
+        Console.WriteLine("A" > "B")
+        'It looks at the characters from left to right and evaluates them. It doesn't add or multiply them
+        Console.WriteLine("AAA" < "ABA")
+        Console.WriteLine("abc" > "ABC")
 
         Console.Read()
 
